6e637c3354096791bbddbbcd6349c36414949048
Jonathan Leo Doorn <jdoorn@purdue.edu>
2020-11-16 19:27:46 -0500

    built database

--- create.sql
+++ create.sql
@@ -2,17 +2,22 @@ CREATE TABLE ra(
     id INTEGER PRIMARY KEY,
     ra_name TEXT,
     ra_career TEXT
-)
+);
 
 CREATE TABLE bed(
     id INTEGER PRIMARY KEY,
-    FOREIGN KEY(ra_id) REFERENCES ra(id),
+    room_num INTEGER,
+    bed_name TEXT,
+    ra1_id INTEGER,
+    ra2_id INTEGER,
     bed_checked_out INTEGER,
-    bed_time_checked_out INTEGER
+    bed_time_checked_out INTEGER,
+    FOREIGN KEY(ra1_id) REFERENCES ra(id),
+    FOREIGN KEY(ra2_id) REFERENCES ra(id)
 );
 
-CREATE TABLE room (
-    room_num INTEGER PRIMARY KEY,
-    FOREIGN KEY(left_id) REFERENCES bed(id),
-    FOREIGN KEY(right_id) REFERENCES bed(id)
-);
+-- CREATE TABLE room (
+--     room_num INTEGER PRIMARY KEY,
+--     FOREIGN KEY(left_id) REFERENCES bed(id),
+--     FOREIGN KEY(right_id) REFERENCES bed(id)
+-- );
